e1283200dc78dbea51ea376218a504183a3c255d
laurent <laurent.neveux@oclock.io>
2025-10-13 08:14:14 +0200

    init

--- client/src/lib/services/auth.service.js
+++ client/src/lib/services/auth.service.js
@@ -1,15 +1,15 @@
 import { httpRequest } from '../httpRequest';
 
 export const registerUser = async (user) => {
-    return await httpRequest('/auth/register', 'POST', user);
+    return await httpRequest('auth/register', 'POST', user);
 };
 
 export const loginUser = async (credentials) => {
-    const { token } = await httpRequest('/auth/login', 'POST', credentials);
+    const { token } = await httpRequest('auth/login', 'POST', credentials);
 
     return { token };
 };
 
 export const getUser = async () => {
-    return await httpRequest('/auth/me', 'GET');
+    return await httpRequest('auth/me', 'GET');
 };
